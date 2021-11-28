170a6097605235544fcd388817f2199251207943
Nxkki <nikkio358@yahoo.com>
2021-11-28 21:52:00 +0000

    Theme & String Resources, Layout, Event Handling 1.0

--- app/src/main/java/org/wit/accommodation/AccommodationActivity.kt
+++ app/src/main/java/org/wit/accommodation/AccommodationActivity.kt
@@ -2,18 +2,40 @@ package org.wit.accommodation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import org.wit.accommodation.databinding.ActivityAccommodationBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 
 class AccommodationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAccommodationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_accommodation)
+//        setContentView(R.layout.activity_accommodation)
+        binding = ActivityAccommodationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         Timber.plant(Timber.DebugTree())
 
         i("Accommodation Activity started..")
+
+//        binding.btnAdd.setOnClickListener() {
+//            i("add Button Pressed")
+//        }
+        binding.btnAdd.setOnClickListener() {
+            val accommodationLocation = binding.accommodationLocation.text.toString()
+            if (accommodationLocation.isNotEmpty()) {
+                i("add Button Pressed: $accommodationLocation")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a Location", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
 
     }
 }
